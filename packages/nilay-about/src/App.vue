796967616dd43cebea39a1aa04bba6a0890b2cf9
Yoshiyuki Sasaki <34340317+sasaki264@users.noreply.github.com>
2019-06-30 13:17:09 +0900

    feat(nilay-about): create home page (#1)
    Nilay-Commit: 83098f4e8def34d6303ea2822aa374476fb08485

--- packages/nilay-about/src/App.vue
+++ packages/nilay-about/src/App.vue
@@ -1,31 +1,71 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar type="light" sticky="true" class="py-0 bg-white l_navbar">
+      <div class="container">
+        <b-navbar-brand href="/">Nilay/About</b-navbar-brand>
+        <b-nav class="mr-auto ml-2">
+          <b-nav-item to="/">ご案内</b-nav-item>
+        </b-nav>
+      </div>
+    </b-navbar>
+    <router-view />
+
+    <footer class="bg-light text-dark">
+      <div class="container py-3">
+        <div class="row">
+          <div class="col-12 h2">
+            <a href="https://www.facebook.com/NilaySport/" class="text-dark" target="_blank">
+              <i class="fab fa-facebook-square"></i>
+            </a>&nbsp;
+            <a href="https://twitter.com/NilayJP" class="text-dark" target="_blank">
+              <i class="fab fa-twitter"></i>
+            </a>&nbsp;
+            <a
+              href="https://www.youtube.com/channel/UC03yJGn_rZV2MTpr-ZrMZrA"
+              class="text-dark"
+              target="_blank"
+            >
+              <i class="fab fa-youtube"></i>
+            </a>&nbsp;
+            <a href="https://www.instagram.com/NilayJP/" class="text-dark" target="_blank">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </div>
+          <hr class="col-12 divider" />
+          <div class="col-3">
+            <h5>Nilay/About</h5>
+          </div>
+          <div class="col-3">
+            <h5>サービス</h5>
+            <div class="pl-3">Knowledge</div>
+            <div class="pl-3">E-commerce</div>
+            <div class="pl-3">Gunman</div>
+          </div>
+          <hr class="col-12 divider" />
+          <div class="col-12 small">
+            <small>© 2019 Nilay</small>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Roboto", "Noto Sans JP", -apple-system, BlinkMacSystemFont,
+    Roboto, "游ゴシック体", YuGothic, "Yu Gothic", "Avenir", Helvetica, Arial,
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
+.l_navbar {
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.navbar .router-link-active {
+  border-bottom: 2px solid rgb(232, 118, 0);
 }
 </style>
