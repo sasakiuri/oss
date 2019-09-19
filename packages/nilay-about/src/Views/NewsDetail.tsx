eea00ca85b388222ffa2bf09cfc914644dbb3c8a
Yoshiyuki Sasaki <34340317+sasaki264@users.noreply.github.com>
2019-09-20 04:54:25 +0900

    feat(nilay-about): create news page (#6)
    Nilay-Commit: 8491a1b95582ebb21efc7b8555ba45cfd0520333

---
+++ packages/nilay-about/src/Views/NewsDetail.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+
+interface News {
+    id: string
+    title: string
+    datetime: string
+    body: string
+}
+
+interface Props extends RouteComponentProps<{ id: string }> { }
+
+interface State {
+    news: News | null
+}
+
+class NewsDetail extends React.Component<Props, State> {
+
+
+    public constructor(props: Props) {
+        super(props)
+        this.state = {
+            news: null
+        }
+    }
+
+    public componentDidMount(): void {
+        axios
+            .get<News>(
+                `https://jq9dz9fa6d.execute-api.ap-northeast-1.amazonaws.com/v1/news/${this.props.match.params.id}`
+            )
+            .then(
+                response => {
+                    console.log(response)
+                    this.setState(
+                        {
+                            news: response.data
+                        }
+                    )
+
+                    if (this.state.news !== null) {
+                        document.title = `${this.state.news.title}：お知らせ：Nilay/Knowledge`
+                    }
+
+                }
+            )
+            .catch(
+                error => { alert(error) }
+            )
+    }
+
+    public render(): React.ReactNode {
+        return (
+            <div id="NewsDetail" className="text-left">
+                {(() => {
+                    if (this.state.news !== null) {
+                        return (
+                            <>
+                                <div className="container">
+                                    <div className="h1">{this.state.news.title}</div>
+                                    <div className="text-right"><small>{this.state.news.datetime}</small></div>
+                                    <div dangerouslySetInnerHTML={{ __html: this.state.news.body }}></div>
+                                </div>
+                            </>
+                        )
+                    }
+                })()}
+            </div>
+        )
+    }
+}
+
+export default NewsDetail;
--- packages/nilay-about/src/Views/NewsView.tsx
+++ packages/nilay-about/src/Views/NewsView.tsx
@@ -1,15 +1,70 @@
 import * as React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-class NewsView extends React.Component {
+interface News {
+    id: string
+    title: string
+    datetime: string
+    body: string
+}
+
+interface Props { }
+
+interface State {
+    newsCollection: Array<News>
+}
+
+class NewsView extends React.Component<Props, State> {
+
+
+    public constructor(props: Props) {
+        super(props);
+        this.state = {
+            newsCollection: new Array<News>()
+        };
+    }
 
     public componentDidMount(): void {
         document.title = "お知らせ：Nilay/Knowledge";
+        axios
+            .get(
+                'https://jq9dz9fa6d.execute-api.ap-northeast-1.amazonaws.com/v1/news/'
+            )
+            .then(
+                response => {
+                    this.setState({
+                        newsCollection: response.data as Array<News>
+                    });
+                }
+            )
+            .catch(
+                error => { alert(error) }
+            )
     }
 
     public render(): React.ReactNode {
         return (
             <div id="NewsView">
-                お知らせページです。
+                <div className="h1">お知らせ</div>
+                <div className="container">
+
+                    <div className="list-group text-left">
+
+                        {this.state.newsCollection.map((news) => {
+                            return (
+                                <Link to={`news/${news.id}`} key={news.id} className="list-group-item list-group-item-action flex-column align-items-start">
+                                    <div className="d-flex w-100 justify-content-between">
+                                        <h5 className="mb-1">{news.title}</h5>
+                                        <small>{news.datetime}</small>
+                                    </div>
+                                    <p className="mb-1 mt-2">{news.body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')}</p>
+                                    <small>詳しく見る</small>
+                                </Link>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         );
     }
