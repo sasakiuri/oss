2fa15a45f157aecfca5f2e3b8ed79409f54ebf24
Yoshiyuki Sasaki <34340317+sasaki264@users.noreply.github.com>
2020-07-04 19:10:52 +0900

    fix(nilay-about): resolve build errors (#24)
    Nilay-Commit: c0b4ffc522e4c580eab836f4c6d1cc48a0dea24e

--- packages/nilay-about/src/adapter/gateway/news-list/web-api-news-list-gateway.ts
+++ packages/nilay-about/src/adapter/gateway/news-list/web-api-news-list-gateway.ts
@@ -1,26 +1,10 @@
 import { News } from "../../../domain/model"
 import { Response, NewsListGateway } from "../../../use-case/news-list"
-import * as firebase from "firebase/app"
-import "firebase/firestore"
-import "firebase/analytics"
+import firebase from "gatsby-plugin-firebase"
 
 export class WebApiNewsListGateway implements NewsListGateway {
   async read(): Promise<Response> {
     try {
-      if (!firebase.apps.length) {
-        firebase.initializeApp({
-          apiKey: "REDACTED_FIREBASE_API_KEY",
-          authDomain: "nilay-about.firebaseapp.com",
-          databaseURL: "https://nilay-about.firebaseio.com",
-          projectId: "nilay-about",
-          storageBucket: "nilay-about.appspot.com",
-          messagingSenderId: "501712650959",
-          appId: "1:501712650959:web:191a29b977cad3f2472dba",
-          measurementId: "G-C3KJX5WQEM",
-        })
-        firebase.analytics()
-      }
-
       const firestore: firebase.firestore.Firestore = firebase.firestore()
       const snapshot: firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData> = await firestore
         .collection("news")
